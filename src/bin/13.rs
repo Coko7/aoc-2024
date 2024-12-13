ee81834b4e5164e147e0711ef032505b21e81034
Coko <91132775+Coko7@users.noreply.github.com>
2024-12-13 17:36:48 +0100

    refactor(day13): clean up code and had test value for part 2

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -5,7 +5,7 @@ struct Machine {
     btn_a: (u64, u64),
     btn_b: (u64, u64),
     prize: (u64, u64),
-    offset: u64,
+    prize_offset: u64,
 }
 
 fn parse_button(input: &str) -> (u64, u64) {
@@ -15,7 +15,6 @@ fn parse_button(input: &str) -> (u64, u64) {
         .collect();
 
     assert_eq!(parts.len(), 2);
-
     (*parts.first().unwrap(), *parts.last().unwrap())
 }
 
@@ -24,13 +23,13 @@ fn parse_prize(input: &str) -> (u64, u64) {
         .split(",")
         .map(|p| p.split("=").last().unwrap().parse::<u64>().unwrap())
         .collect();
-    assert_eq!(parts.len(), 2);
 
+    assert_eq!(parts.len(), 2);
     (*parts.first().unwrap(), *parts.last().unwrap())
 }
 
 impl Machine {
-    fn new(input: &str, offset: u64) -> Machine {
+    fn new(input: &str, prize_offset: u64) -> Machine {
         let mut lines = input.lines();
 
         let btn_a = parse_button(lines.next().unwrap());
@@ -41,101 +40,76 @@ impl Machine {
             btn_a,
             btn_b,
             prize,
-            offset,
+            prize_offset,
         }
     }
 
-    fn real_prize(&self) -> (u64, u64) {
-        (self.prize.0 + self.offset, self.prize.1 + self.offset)
-    }
-
-    fn new_machines(input: &str, offset: u64) -> Vec<Machine> {
+    fn new_machines(input: &str, prize_offset: u64) -> Vec<Machine> {
         input
             .split("\n\n")
-            .map(|s| Machine::new(s, offset))
+            .map(|s| Machine::new(s, prize_offset))
             .collect()
     }
 
-    fn calc_presses(&self, a: u64, b: u64) -> (u64, u64) {
-        let x = self.btn_a.0 * a + self.btn_b.0 * b;
-        let y = self.btn_a.1 * a + self.btn_b.1 * b;
-        (x, y)
+    fn real_prize(&self) -> (u64, u64) {
+        (
+            self.prize.0 + self.prize_offset,
+            self.prize.1 + self.prize_offset,
+        )
     }
 
-    fn try_smart_solve(&self) -> Option<(u64, u64)> {
+    fn find_optimal_presses(&self) -> Option<(u64, u64)> {
         let (a0, a1) = (self.btn_a.0 as f64, self.btn_a.1 as f64);
         let (b0, b1) = (self.btn_b.0 as f64, self.btn_b.1 as f64);
 
-        let prize = self.real_prize();
-        let (z0, z1) = (prize.0 as f64, prize.1 as f64);
+        // Real prize is relevant for part 2 where an offset is used
+        // For part 1, offset is 0 so self.prize = self.real_prize()
+        let real_prize = self.real_prize();
+        let (z0, z1) = (real_prize.0 as f64, real_prize.1 as f64);
 
-        let a_presses = (b1 * z0 - b0 * z1) / (a0 * b1 - a1 * b0);
-        let b_presses = (z0 - a0 * a_presses) / b0;
+        // Isolate n and m in following equations:
+        // - z0 = a0 * n + b0 * m
+        // - z1 = a1 * n + b1 * m
+        // Where:
+        // - n: how many times to press A button
+        // - m: how many times to press B button
+        let n = (b1 * z0 - b0 * z1) / (a0 * b1 - a1 * b0);
+        let m = (z0 - a0 * n) / b0;
 
-        if a_presses.fract() != 0.0 || b_presses.fract() != 0.0 {
+        // If n and m have fractional part, that means no solution with 'integer' numbers
+        if n.fract() != 0.0 || m.fract() != 0.0 {
             return None;
         }
 
-        Some((a_presses as u64, b_presses as u64))
+        Some((n as u64, m as u64))
     }
 
-    fn try_solve(&self) -> Option<(u64, u64)> {
-        let mut a = self.offset;
-        let mut b = self.offset;
-
-        loop {
-            loop {
-                let x = self.btn_a.0 * a + self.btn_b.0 * b;
-                let y = self.btn_a.1 * a + self.btn_b.1 * b;
-
-                let prize = self.real_prize();
-                if x > prize.0 || y > prize.1 {
-                    break;
-                }
-
-                if x == prize.0 && y == prize.1 {
-                    return Some((a, b));
-                }
-
-                b += 1;
-            }
-            a += 1;
-            b = 0;
-
-            let res = self.calc_presses(a, b);
-            if res.0 > self.prize.0 || res.1 > self.prize.1 {
-                break;
-            }
+    fn find_prize_cost(&self) -> Option<u64> {
+        match self.find_optimal_presses() {
+            // Pressing A button costs 3 tokens
+            // Pressing B button costs 1 token
+            Some((a, b)) => Some(a * 3 + b * 1),
+            None => None,
         }
-
-        None
     }
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let machines = Machine::new_machines(input, 0);
-
-    let mut tokens = 0;
-    for machine in machines.iter() {
-        if let Some(sol) = machine.try_smart_solve() {
-            tokens += sol.0 * 3 + sol.1 * 1
-        }
-    }
-
-    Some(tokens)
+    Some(
+        Machine::new_machines(input, 0)
+            .iter()
+            .map(|m| m.find_prize_cost().unwrap_or(0))
+            .sum::<u64>(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let machines = Machine::new_machines(input, 10000000000000);
-
-    let mut tokens = 0;
-    for machine in machines.iter() {
-        if let Some(sol) = machine.try_smart_solve() {
-            tokens += sol.0 * 3 + sol.1 * 1
-        }
-    }
-
-    Some(tokens)
+    Some(
+        Machine::new_machines(input, 10000000000000)
+            .iter()
+            .map(|m| m.find_prize_cost().unwrap_or(0))
+            .sum::<u64>(),
+    )
 }
 
 #[cfg(test)]
@@ -151,6 +125,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(875318608908));
     }
 }
